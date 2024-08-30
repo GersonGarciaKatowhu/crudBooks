5200fe0223076018bd9e1bf415556decb3badc5a
Katowhu <garcia.roman.gerson@gmail.com>
2024-08-30 12:15:57 -0600

    fix a render problems

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -3,7 +3,7 @@ export const Star = ({ filled }) => {
     <svg
       height="30.5px"
       width="30.3px"
-      fill={filled ? "gold" : "gray"}
+      fill={filled ? "gold" : "aquamarine"}
       viewBox="0 0 25 25"
     >
       <polygon points="9.9, 1.1, 8, 7.4, 1.6, 7.4, 7.1, 11.1, 5.2, 17.3, 9.9, 13.7, 14.6, 17.3, 12.7, 11.1, 18.2, 7.4, 11.8, 7.4 " />
